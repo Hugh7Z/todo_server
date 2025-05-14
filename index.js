9c5f55162b65d80d3305baccd668585abeaeeafc
hugh <hugh@Mac.local>
2025-05-14 11:58:59 +0800

    mongodb connection

--- index.js
+++ index.js
@@ -10,9 +10,12 @@ app.use(cors());
 app.use(express.json());
 
 // 连接MongoDB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/todo')
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected successfully'))
+.catch(err => console.error('MongoDB connection error:', err));
 
 // Todo模型
 const todoSchema = new mongoose.Schema({
